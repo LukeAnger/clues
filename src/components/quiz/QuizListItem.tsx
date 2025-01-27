135d123b422b8f7c811aa9783a8e60b2d3b50a34
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2025-01-26 21:02:53 -0500

    Updated this component to map answers rather than DRY

--- src/components/quiz/QuizListItem.tsx
+++ src/components/quiz/QuizListItem.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { IonItem, IonLabel, IonCheckbox, IonCardContent } from "@ionic/react";
-import { QuizListItemProps } from "./quizTypings";
+import { QuizListItemProps, AnswerKey } from "./quizTypings";
 import Answer from "./Answer";
 import Explanation from "./Explanation";
 import Hint from "./Hint";
 
 const QuizListItem: React.FC<QuizListItemProps> = ({ question, handleSelectAnswer, currSelectedAnswer, showExplanation, handleShowExplanation }) => {
-    console.log("QuizListItem Question: ", question);
+    // console.log("QuizListItem Question: ", question);
     const handleIsCorrect = (answer: string) => {
         if (answer === question["Correct Answer"]) {
             return true;
@@ -17,53 +17,24 @@ const QuizListItem: React.FC<QuizListItemProps> = ({ question, handleSelectAnswe
     return (
         <IonCardContent id="quiz-card-content" >
             
-            <div id="question">{question.Question}</div>
-
-            <div id="answers">
-                <Answer 
-                    answer={{
-                        letter: "A", 
-                        option: question.A, 
-                        selected: currSelectedAnswer === "A" ? true : false, 
-                        correct: handleIsCorrect("A")
-                    }} 
-                    handleSelectAnswer={handleSelectAnswer} 
-                />
-
-                <Answer 
-                    
-                    answer={{
-                        letter: "B", 
-                        option: question.B, 
-                        selected: currSelectedAnswer === "B" ? true : false, 
-                        correct: handleIsCorrect("B")
-                    }}  
-                    handleSelectAnswer={handleSelectAnswer} 
-                />
-
-                <Answer 
-                    
-                    answer={{
-
-                        letter: "C", 
-                        option: question.C, 
-                        selected: currSelectedAnswer === "C" ? true : false, 
-                        correct: handleIsCorrect("C")
-                    }}  
-                    handleSelectAnswer={handleSelectAnswer} 
-                />
-
-                <Answer 
-                    
-                    answer={{
-                        letter: "D", 
-                        option: question.D, 
-                        selected: currSelectedAnswer === "D" ? true : false, 
-                        correct: handleIsCorrect("D")
-                    }}  
-                    handleSelectAnswer={handleSelectAnswer} 
-                />
+            <div id="question">
+                {question.Question}
             </div>
+
+            
+            {(["A", "B", "C", "D"] as AnswerKey[]).map((letter) => (
+                <Answer
+                    key={letter}
+                    answer={{
+                        letter,
+                        option: question[letter],
+                        selected: currSelectedAnswer === letter,
+                        correct: handleIsCorrect(letter),
+                    }}
+                    handleSelectAnswer={handleSelectAnswer}
+                    currSelectedAnswer={currSelectedAnswer}
+                />
+            ))}
                     
         </IonCardContent>
     );
