9167815afb3ff354a2c31ff6a8d1fbcbf2d51420
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2025-01-26 21:02:00 -0500

    minor translation adjustment

--- src/components/quiz/Hint.tsx
+++ src/components/quiz/Hint.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { HintProps } from './quizTypings';
 
 const Hint: React.FC<HintProps> = ({hint, showHint}) => {
-    console.log("Hint", hint, showHint);
+    // console.log("Hint", hint, showHint);
     const style = {
-        transform: showHint ? "translateY(-110%)" : "translateY(10%)"
+        transform: showHint ? "translateY(-105%)" : "translateY(10%)"
     }
     return (
         <div style={style} id="hint">
