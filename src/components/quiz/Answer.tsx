09095479513cde1f7b3c00233abad0afdf9a2998
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2025-01-26 21:01:26 -0500

    Updated Answer
    Changed some styling and conditional checks to ensure correct answer is shown when student selects incorrect answer. disabling all answer components once an answer has been selected

--- src/components/quiz/Answer.tsx
+++ src/components/quiz/Answer.tsx
@@ -2,24 +2,32 @@ import React from "react";
 import { IonModal, IonButton } from "@ionic/react";
 import { AnswerProps } from "./quizTypings";
 import Explanation from "./Explanation";
-const Answer: React.FC<AnswerProps> = ({answer, handleSelectAnswer}) => {
+const Answer: React.FC<AnswerProps> = ({answer, handleSelectAnswer, currSelectedAnswer}) => {
     const { letter, option, selected, correct } = answer;
+    const disabled = currSelectedAnswer !== "" ? "none" : "auto";
     // console.log("Answer: ", answer);
-    const correctStyle = { backgroundColor: "green" };
-    const incorrectStyle = { backgroundColor: "red" };
+    const correctStyle = { boxShadow: "0px 0px 5px 2px green", pointerEvents: disabled };
+    const incorrectStyle = { boxShadow: "0px 0px 5px 2px red", pointerEvents: disabled };
+    const disabledStyle = { pointerEvents: disabled };
 
     const style = {};
-
-    if (selected) {
-        if (correct) {
+    if (currSelectedAnswer !== "") {
+        console.log("Selected Answer: ", currSelectedAnswer);
+        if (selected && !correct) {
+            Object.assign(style, incorrectStyle);
+        } else if (correct) {
             Object.assign(style, correctStyle);
         } else {
-            Object.assign(style, incorrectStyle);
+            console.log("Not selected, not correct");
+            Object.assign(style, disabledStyle);
         }
     }
     return (
-        <div style={style} className="answer" onClick={() => handleSelectAnswer(letter)}>
-            {option}
+        <div style={style} className="answer-container">
+            <div  className="answer" onClick={() => handleSelectAnswer(letter)}>
+                {option}
+            </div>
+
         </div>
     );
 };
